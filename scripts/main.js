af6f1c585cbac9a0895f1cc11c559a09a6a11ece
jonas <jonas-gruempel@gmx.de>
2020-11-03 23:17:47 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,19 @@ var responsiveDesign = new $.Deferred();
 
 (function() {
     var responsive = new $.Deferred();
+
     window.addEventListener("load", makeReponsive);
     window.addEventListener("resize", makeReponsive);
 
     function makeReponsive() {
-        var book = $("#book");
-        var width = book.width();
-        book.css("height", width * 1.5);
+        var objs = $(".responsible-obj");
+        var width = objs.width();
+        objs.css("height", width * 1.5);
         if (responsive.state() == "pending") {
             responsive.resolve();
         }
     }
 
+    responsive.then(responsiveDesign.resolve);
+    
 })();
---
+++ scripts/touch-input.js
@@ -0,0 +1,83 @@
+class TouchInput {
+    
+    constructor() {
+        this.oContainer = $(document.body);
+        this.sSwipeDir;
+        this.nStartX;
+        this.nStartY;
+        this.nDistX;
+        this.nDistY;
+        this.nThreshold = 150;
+        this.nRestraint = 100;
+        this.nAllowedTime = 300;
+        this.nElapsedTime;
+        this.nStartTime;
+        this.fnSwipeHandler = function(){};
+    }
+
+    onSwipe(fnHandler) {
+
+        this.fnSwipeHandler = fnHandler;
+
+    }
+
+    render() {
+
+        var oOverlay = $('<div>', {
+            class: 'shadow'
+        });
+
+        oOverlay.on('touchstart', oEvent => {
+            var oTouch = oEvent.changedTouches[0];
+            this.sSwipeDir = 'none';
+            this.nStartX = oTouch.pageX;
+            this.nStartY = oTouch.pageY;
+            this.nStartTime = new Date().getTime() ;
+            oEvent.preventDefault();
+    
+        });
+
+        oOverlay.on('touchmove', oEvent => {
+            oEvent.preventDefault();
+        });
+
+        oOverlay.on('touchend', oEvent => {
+            var oTouch = oEvent.changedTouches[0]
+            this.nDistX = oTouch.pageX - this.nStartX 
+            this.nDistY = oTouch.pageY - this.nStartY 
+            this.nElapsedTime = new Date().getTime() - this.nStartTime 
+            if (this.nElapsedTime <= this.nAllowedTime){ 
+                if (Math.abs(this.nDistX) >= this.nThreshold && Math.abs(this.nDistY) <= this.nRestraint){
+                    this.sSwipeDir = (this.nDistX < 0)? 'left' : 'right' 
+                }
+                else if (Math.abs(this.nDistY) >= this.nThreshold && Math.abs(this.nDistX) <= this.nRestraint){ 
+                    this.sSwipeDir = (this.nDistY < 0)? 'up' : 'down'
+                }
+            }
+
+            if (this.sSwipeDir && this.sSwipeDir != 'none') {
+                this.fnSwipeHandler(this.sSwipeDir)
+            }
+            
+            oEvent.preventDefault()
+    
+        });
+
+        
+        oOverlay.appendTo(this.oContainer);
+
+    }
+
+}
+
+var ti = new TouchInput();
+
+ti.onSwipe((dir) => {
+    if (dir == 'left') {
+        $('.flipable-site-inner').css('transform', 'rotateY(-150deg)');
+    } else if (dir == 'right') {
+        $('.flipable-site-inner').css('transform', 'rotateY(0deg)');
+    }
+})
+
+ti.render();
